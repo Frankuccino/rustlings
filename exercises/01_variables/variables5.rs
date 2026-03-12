45c8effa874f67ada28036571d18af0231358546
Frank Angelo Malubag <frankabad189@gmail.com>
2026-03-12 18:43:38 +0800

    rustlings: completed variables exercises

--- exercises/01_variables/variables5.rs
+++ exercises/01_variables/variables5.rs
@@ -3,6 +3,8 @@ fn main() {
     println!("Spell a number: {number}");
 
     // TODO: Fix the compiler error by changing the line below without renaming the variable.
-    number = 3;
+    let number = 3;
     println!("Number plus two is: {}", number + 2);
 }
+// This is to do shadowing by assigning 'let' on line 6
+// Shadowing allows a change in value and type and new ownership
--- exercises/01_variables/variables6.rs
+++ exercises/01_variables/variables6.rs
@@ -1,6 +1,7 @@
 // TODO: Change the line below to fix the compiler error.
-const NUMBER = 3;
+const NUMBER: u32 = 3;
 
 fn main() {
     println!("Number: {NUMBER}");
 }
+// By assigning type annotation to the constant variable as it is required.
