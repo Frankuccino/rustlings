69e3ec112c70bc8a5db30678fc619013f222ec36
Frank Angelo Malubag <frankabad189@gmail.com>
2026-04-02 21:41:09 +0800

    RE: Fixing clippy: 'unnecessary_fold' by using the suggestion '.product' on the inclusive range to do a factorial rather than the use of the .fold() method to do some accumulator and multiplying that with the element in the closure body to simulate a factorial.

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -12,7 +12,8 @@ fn factorial(num: u64) -> u64 {
     // - recursion
 //    (1..num).fold(1, |acc, x| acc * x)
 // Make use of inclusive range which starts at 1 then include up to num
-    (1..=num).fold(1, |acc, x| acc * x)
+    // (1..=num).fold(1, |acc, x| acc * x)
+    (1..=num).product()
 }
 
 fn main() {
